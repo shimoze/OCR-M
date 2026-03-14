3e53d3cb0dd5e437b6aa2fd1068a278646b322d0
Shimoze <btslover189cdf@gmail.com>
2026-03-15 00:02:07 +0500

    feat: integrate OCRLogger into debug pipeline

--- app/debug.py
+++ app/debug.py
@@ -1,8 +1,56 @@
-from utils.visualization import OCRVisualizer
-from utils.paths import RAW_DIR
 import cv2
+from datetime import datetime
 
-viz = OCRVisualizer()
+from utils.visualization import OCRVisualizer
+from utils.paths import DEBUG_DIR, RAW_DIR
+from utils.logger import OCRLogger
+
+from ocr.engine import init_ocr, run_ocr
+from ocr.layout import sort_boxes, group_lines
+from ocr.postprocess import postprocess
+from ocr.preprocessing import soft_preprocess
+
+"""
+    Подготовка папки
+"""
+
+timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+run_dir = DEBUG_DIR / timestamp
+run_dir.mkdir(parents=True, exist_ok=True)
+
+steps_dir = run_dir / "steps_dir"
+steps_dir.mkdir(parents=True, exist_ok=True)
+
+"""
+    Реализация логгера 
+"""
+img_path = RAW_DIR / "test_image.jpg"
+
+img = cv2.imread(str(img_path))
+
+logger = OCRLogger(run_dir, total_steps=6)
+logger.setup()
+
+with logger.log_step("preprocess"):
+    img = soft_preprocess(img)
+
+with logger.log_step("init ocr"):
+    ocr = init_ocr()
+
+with logger.log_step("run ocr"):
+    items = run_ocr(ocr, img)
+
+with logger.log_step("sort boxes"):
+    items = sort_boxes(items)
+
+with logger.log_step("group lines"):
+    lines = group_lines(items)
+
+with logger.log_step("postprocess"):
+    text = postprocess(lines)
+
+"""Часть с визуализатором"""
+viz = OCRVisualizer(steps_dir)
 
 img = cv2.imread(RAW_DIR / "test_image.jpg")
 viz.save("original", img)
@@ -28,5 +76,3 @@ viz.save_boxes(morph, boxes)
 # показать grid
 viz.show_grid(cols=3)
 
-# вывести тайминги
-viz.print_timings()
